5847fa634dad8297953b8e9c8bf428a4a0afadd3
Samuel Bear Powell <samuel.powell@uq.edu.au>
2019-04-08 11:47:03 +1000

    rewrite spi_slave so that ssel and sclk are synchronized to clk first, then the shift registers are written in the sclk clock domain.

--- spi_slave.v
+++ spi_slave.v
@@ -5,37 +5,35 @@ spi_slave.v
 2019-03-29 Samuel B Powell
 samuel.powell@uq.edu.au
 
-Basic SPI slave with a few protocol options. Most
-notably, MISO and MOSI may have different phases.
+Basic SPI slave with a few protocol options. Most notably, MISO and MOSI may have different phases.
+
+This module includes synchonizers for ssel and sclk to clk controlled by the
+SyncStages parameter. If you don't want synchronization, set SyncStages to 0.
+If SyncStages is nonzero, be sure that clk is sufficiently faster than sclk that
+the protocol timing is not overly disturbed. With SyncStages == 2, clk should be
+at least N times faster than sclk.
 
 While SSEL is asserted, every sclk cycle shifts another bit in/out.
-As soon as WordWidth bits are shifted in, or SSEL is unasserted, mosi_valid is
- pulsed for a single clk cycle--indicating that it may be read.
+As soon as WordWidth bits are shifted in, mosi_valid is pulsed for a single clk
+ cycle--indicating that it may be read. If SSEL is deasserted before all of the
+ bits are read, the SPI slave rejects the read and resets.
 When SSEL is asserted the slave registers miso_word_in to shift out. As soon as
 WordWidth bits are shifted out, the next word is accepted.
-if SSEL is deasserted before WordWidth bits are shifted and while sclk == CPOL,
- and OUTPUT_PARTIAL is nonzero, then mosi_valid will pulsed. Otherwise, partial
- transfers are discarded.
 
 MISO is provided via a basic interface: when a transfer begins (ssel is asserted)
 miso_word_in is latched and miso_accepted is asserted for a single cycle. Every time
 WordWidth bits are shifted out, this repeats. This allows a first-word fall-through
 FIFO to easily provide data to the slave.
 
-NB: Assumes that select, sclk, and mosi are externally synchronized to clk.
-This module also does not attempt clock recovery, so miso will lag sclk by a
- cycle of clk -- to avoid timing issues, clk must be sufficiently faster than
- sclk!
-
 spi_slave #(
     .WordWidth(8), // bits per word
     .IndexWidth(3), // ceil(log2(WordWidth))
+    .SyncStages(2), // synchronizer stages for ssel and sclk
     .SPOL(0), // 0 or 1 -- if 0, SSEL is active low (typical)
     .CPOL(0), // 0 or 1 -- sclk phase
     .MOSI_PHA(0), // 0 or 1 -- if 0, MOSI shifts in on the lagging sclk edge
     .MISO_PHA(0), // 0 or 1 -- if 0, MISO shifts out on the leading sclk edge
-    .MSB_FIRST(1), // if nonzero, shift the MSb in/out first
-    .OUTPUT_PARTIAL(0) // if nonzero, trigger mosi_valid on partial transfers
+    .MSB_FIRST(1) // if nonzero, shift the MSb in/out first
 ) spi_slave_0 (
     .clk(), // system clock
     .reset(), // system reset
@@ -53,20 +51,20 @@ spi_slave #(
 module spi_slave #(
     parameter WordWidth = 8,
     parameter IndexWidth = 3,
+    parameter SyncStages = 2,
     parameter SPOL = 0, // 0 or 1 -- if 0, SSEL is active low (typical)
     parameter CPOL = 0, // 0 or 1 -- sclk phase
     parameter MOSI_PHA = 0, // 0 or 1 -- if 0, MOSI shifts in the lagging sclk edge
     parameter MISO_PHA = 0,  // 0 or 1 -- if 0, MISO shifts out on the leading sclk edge
-    parameter MSB_FIRST = 1, // if non-zero, shift the MSb in/out first
-    parameter OUTPUT_PARTIAL = 0 // if non-zero, trigger mosi_valid on partial transfers
+    parameter MSB_FIRST = 1 // if non-zero, shift the MSb in/out first
 )(
     input wire clk,
     input wire reset,
 
-    output reg mosi_valid, //pulse: mosi_word can be read
+    output wire mosi_valid, //pulse: mosi_word can be read
     output reg [WordWidth-1:0] mosi_word,
     input wire [WordWidth-1:0] miso_word_in,
-    output reg miso_accepted,
+    output wire miso_accepted,
 
     input wire ssel,
     input wire sclk,
@@ -74,25 +72,56 @@ module spi_slave #(
     output wire miso 
 );
 
-//states
-localparam IDLE = 0;
-localparam RUN = 1;
-localparam END = 2;
+//normalize ssel to active high:
+//normalize and gate sclk:
+wire ssel_norm = (ssel == SPOL[0]);
+wire sclk_norm = ~(ssel_norm && (sclk == CPOL));
+//synchronize ssel and sclk in their normalized forms
+wire ssel_sync, sclk_sync; 
+synchronizer #(
+    .Width(2), 
+    .Stages(SyncStages),   //number of shift registers
+    .Init(0),     //if nonzero, initialize each register to InitValue
+    .InitValue(2'b01) //initial & reset value
+) ssel_sclk_sync (
+    .clk(clk),   //in: output clock
+    .reset(reset), //in: active high reset
+    .in({ssel_norm, sclk_norm}), //in [Width]: data in
+    .out({ssel_sync, sclk_sync}) //out [Width]: data out, delayed by Stages clk cycles
+);
 
-reg [1:0] state, state_D;
+//state
+reg idle, idle_D;
+reg last, last_D;
 reg [IndexWidth-1:0] cycle, cycle_D;
 reg skip, skip_D;
 
-reg mosi_valid_D, miso_accepted_D;
+//we have to do this double register thing to get a proper one-shot pulse in
+// the clk domain
+reg miso_ack, miso_ack_D;
+reg miso_ack_gate, miso_ack_gate_D;
+assign miso_accepted = miso_ack & miso_ack_gate;
+
+reg mosi_val, mosi_val_D;
+reg mosi_val_gate, mosi_val_gate_D;
+assign mosi_valid = mosi_val & mosi_val_gate;
+
 reg [WordWidth-1:0] miso_word, miso_word_D;
 reg [WordWidth-1:0] mosi_word_D;
-reg prev_sclk; //shift register to find edges in sclk
+
+function [WordWidth-1:0] shift;
+    input [WordWidth-1:0] word;
+    input bit;
+    if(MSB_FIRST == 0) shift = {bit, word[WordWidth-1:1]};
+    else shift = {word[WordWidth-2:0], bit};
+endfunction
 
 generate
-if(MSB_FIRST == 0)
+if(MSB_FIRST == 0) begin
     assign miso = miso_word[0];
-else
+end else begin
     assign miso = miso_word[WordWidth-1];
+end
 endgenerate
 
 //combinational logic
@@ -100,132 +129,117 @@ always @* begin
     //defaults
     mosi_word_D = mosi_word;
     miso_word_D = miso_word;
-    miso_accepted_D = 0; //one shot
-    state_D = state;
-    cycle_D = cycle;
+    
+    miso_ack_D = 0;
+    mosi_val_D = 0;
+    miso_ack_gate_D = miso_ack_gate;
+    mosi_val_gate_D = mosi_val_gate;
+
+    idle_D = idle;
+    if(cycle == 0) cycle_D = WordWidth - 1;
+    else cycle_D = cycle - 1;
+    
     skip_D = skip;
-    mosi_valid_D = 0; //one-shot
-    if(mosi_valid == 1) mosi_word_D = 0; //reset mosi
+    if(mosi_val == 1) mosi_word_D = 0; //reset mosi
 
-    case(state)
-    IDLE: begin
+    //one-shot pulses for miso_accepted and mosi_valid
+    if(miso_ack == 1'b1) begin
+        miso_ack_gate_D = 1'b0;
+        mosi_val_gate_D = 1'b1;
+    end
+    if(mosi_val == 1'b1) mosi_val_gate_D = 1'b0;
+
+    if(idle == 1'b1) begin
         //we need to be ready to send data at a moment's notice
+        miso_ack_gate_D = 1'b1;
         miso_word_D = miso_word_in;
+        mosi_word_D = 0;
         cycle_D = WordWidth-1;
+        last_D = 0;
         if(MISO_PHA == 1) skip_D = 1;
-        if(ssel == SPOL[0]) begin
+        if(ssel_sync == 1'b1) begin
             //start a transfer
-            state_D = RUN;
-            miso_accepted_D = 1;
-            mosi_word_D = 0;
+            idle_D = 1'b0;
+            miso_ack_D = 1;
         end
-    end
-    RUN:
-    if(ssel == SPOL[0]) begin
-        if((prev_sclk == CPOL[0]) && (sclk == ~CPOL[0])) begin
-            //sclk: CPOL to ~CPOL (rising edge when CPOL == 0)
-            if(MOSI_PHA == 0) begin
-                //shift next MOSI bit in
-                if(MSB_FIRST == 0)
-                    mosi_word_D = {mosi, mosi_word[WordWidth-1:1]};
-                else
-                    mosi_word_D = {mosi_word[WordWidth-2:0], mosi};
-                if(cycle == 0) mosi_valid_D = 1;
-            end
-            if(MISO_PHA == 1) begin
-                //shift next MISO bit out
-                if(skip == 1) skip_D = 0; //skip the first shift
-                else begin
-                    if(MSB_FIRST == 0)
-                        miso_word_D = {1'b0, miso_word[WordWidth-1:1]};
-                    else
-                        miso_word_D = {miso_word[WordWidth-2:0], 1'b0};
+    end else begin
+        if(last == 1'b0) begin
+            if(ssel_sync == 1'b1) begin
+                mosi_word_D = shift(mosi_word, mosi);
+                
+                if(skip == 1)  begin
+                    skip_D = 0; //skip the first shift
+                    miso_ack_D = 1;
                 end
-            end
+                else miso_word_D = shift(miso_word, 1'b0);
 
-        end else if((prev_sclk == ~CPOL[0]) && (sclk == CPOL[0])) begin
-            //sclk: ~CPOL to CPOL (falling edge when CPOL == 0)
-            if(MOSI_PHA == 1) begin
-                //shift next MOSI bit in
-                if(MSB_FIRST == 0)
-                    mosi_word_D = {mosi, mosi_word[WordWidth-1:1]};
-                else
-                    mosi_word_D = {mosi_word[WordWidth-2:0], mosi};
-                if(cycle == 0) mosi_valid_D = 1;
-            end
-            if(MISO_PHA == 0) begin
-                //shift next MISO bit out
-                if(skip == 1) skip_D = 0; //skip the first shift
-                else begin
-                    if(MSB_FIRST == 0)
-                        miso_word_D = {1'b0, miso_word[WordWidth-1:1]};
-                    else
-                        miso_word_D = {miso_word[WordWidth-2:0], 1'b0};
+                if(cycle == 0) begin 
+                    mosi_val_D = 1;
+                    last_D = 1'b1;
+                    cycle_D = WordWidth-1;
+                    miso_ack_gate_D = 1'b1;
+                    if(MISO_PHA == 0) begin
+                        miso_word_D = miso_word_in;
+                    end
                 end
-            end
-            if(cycle > 0) begin
-                cycle_D = cycle - 1;
             end else begin
-                state_D = END;
-                cycle_D = WordWidth - 1;
-                //if sclk changes again a new cycle would begin
-                //we must prepare
-                if(MISO_PHA == 0) begin
-                    miso_word_D = miso_word_in;
-                end
+                //ssel went low early
+                //quit
+                idle_D = 1'b1;
             end
-        end
-    end else begin //ssel stopped early
-        if(OUTPUT_PARTIAL != 0 && (sclk == CPOL[0])) begin
-            //it stopped in a valid place
-            //we'll output what we've received so far
-            mosi_valid_D = 1;
-        end
-        state_D = IDLE;
-    end
-    END: begin
-        if(ssel == SPOL[0]) begin
-            if((prev_sclk == CPOL[0]) && (sclk == ~CPOL[0])) begin //(rising)
-                //the clock transitioned again, so we carry on with another word
-                miso_word_D = miso_word_in;
-                miso_accepted_D = 1;
-                state_D = RUN;
+        end else begin //last == 1'b1
+            //it's the last sclk low pulse
+            miso_word_D = miso_word_in;
+            if(ssel_sync == 1'b1) begin
+                miso_ack_D = 1'b1;
+                last_D = 1'b0;
                 if(MOSI_PHA == 0) begin
-                    //shift next MOSI bit in
-                    if(MSB_FIRST == 0)
-                        mosi_word_D = {mosi, mosi_word[WordWidth-1:1]};
-                    else
-                        mosi_word_D = {mosi_word[WordWidth-2:0], mosi};
-                    if(cycle == 0) mosi_valid_D = 1;
+                    mosi_word_D = shift(mosi_word, mosi);
+                    if(cycle == 0) mosi_val_D = 1;
                 end
+            end else begin
+                idle_D = 1'b1;
             end
-        end else begin //ssel stopped, as expected
-            state_D = IDLE;
         end
     end
-    endcase
 end
 
-//registers, synchronous reset, always enabled
+//registers on the system clock
 always @(posedge clk) begin
     if(reset) begin
-        prev_sclk <= 0;
-        state <= IDLE;
-        cycle <= 0;
-        skip <= 0;
-        mosi_valid <= 0;
-        mosi_word <= 0;
-        miso_accepted <= 0;
-        miso_word <= 0;
+        idle <= 1'b1;
+        mosi_val_gate <= 1'b0;
+        miso_ack_gate <= 1'b0;
     end else begin
-        prev_sclk <= sclk;
-        state <= state_D;
-        cycle <= cycle_D;
-        skip <= skip_D;
-        mosi_valid <= mosi_valid_D;
+        idle <= idle_D;
+        mosi_val_gate <= mosi_val_gate_D;
+        miso_ack_gate <= miso_ack_gate_D;
+    end
+end
+
+//registers on sclk
+always @(posedge sclk_sync) begin
+    if(MOSI_PHA == 0) begin
         mosi_word <= mosi_word_D;
-        miso_accepted <= miso_accepted_D;
+        mosi_val <= mosi_val_D;
+    end
+    if(MISO_PHA == 1) begin
         miso_word <= miso_word_D;
+        miso_ack <= miso_ack_D;
+    end
+end
+
+always @(negedge sclk_sync) begin
+    skip <= skip_D;
+    cycle <= cycle_D;
+    last <= last_D;
+    if(MOSI_PHA == 1) begin
+        mosi_word <= mosi_word_D;
+        mosi_val <= mosi_val_D;
+    end
+    if(MISO_PHA == 0) begin
+        miso_word <= miso_word_D;
+        miso_ack <= miso_ack_D;
     end
 end
 
