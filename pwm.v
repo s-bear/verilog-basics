8a5b3e8dc3467c631069f1b094f8df1619dd430f
Samuel Bear Powell <samuel.powell@uq.edu.au>
2019-07-16 19:01:12 +1000

    add docs to pwm.v

--- pwm.v
+++ pwm.v
@@ -3,12 +3,37 @@
 pwm.v
 (C) 2019-04-15 Samuel B Powell
 samuel.powell@uq.edu.au
+
+Pulse width modulation with spread-spectrum mode.
+
+When SpreadSpectrum is non-zero, the module reverses the bits of the internal
+counter before comparing to the duty cycle. The ratio of on to off time remains
+the same, but the periods of on time will be scattered evenly within the total 
+counter period.
+e.g: With a 3 bits and a duty cycle of 75% (duty_cycle=5)
+count:  000  001  010  011  100  101  110  111
+normal:  1    1    1    1    1    1    0    0
+spread:  1    1    1    0    1    1    1    0
+
+
+module pwm #(
+    .Bits(8),
+    .ActiveHigh(1),
+    .SpreadSpectrum(1)
+)(
+    .clk(),        // in
+    .reset(),      // in
+    .enable(),     // in
+    .set_duty(),   // in 
+    .duty_cycle(), // in [Bits]: latched when set_duty is asserted
+    .out()         //out: modulated when enable is asserted
+);
 */
 
 module pwm #(
     parameter Bits = 8,
     parameter ActiveHigh = 1,
-    parameter SpreadSpectrum = 1,
+    parameter SpreadSpectrum = 1
 )(
     input wire clk,
     input wire reset,
@@ -23,14 +48,14 @@ reg comp;
 
 integer i;
 always @* begin
-    //reverse the bits of state
-    for(i = 0; i < Bits; i = i + 1)
-        state_rev[i] = state[Bits-1-i];
-
-    if(SpreadSpectrum == 0)
+    if(SpreadSpectrum == 0) begin
         comp = (state <= duty);
-    else
+    end else begin
+        //reverse the bits of state
+        for(i = 0; i < Bits; i = i + 1)
+            state_rev[i] = state[Bits-1-i];
         comp = (state_rev <= duty);
+    end
 end
 
 always @(posedge clk) begin
